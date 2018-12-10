3d6e3eeebf3eecdebbda5d37242fb961f7759b78
Bastien <bastienmmc@gmail.com>
2018-12-10 15:46:23 +0100

    creation fichier app.ps

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
-var http = require('http');
-var fs = require('fs');
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require(socket.io).listen(server),
+    ent = require('ent'); // permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en php)
 
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function (req, res) {
-    fs.readFile('./index.html', 'utf-8', function (error, content) {
-        res.writeHead(200, {
-            "Content-Type": "text/html"
-        });
-        res.end(content);
-    });
+// chargement de la page index.html
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + 'index.html');
 });
-
-// Chargement de socket.io
-var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket, pseudo) {
-    // Quand un client se connecte, on lui envoie un message
-    socket.emit('message', 'Vous êtes bien connecté !');
-    // On signale aux autres clients qu'il y a un nouveau venu
-    socket.broadcast.emit('message', 'Un autre client vient de se connecter ! ');
-
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session
-    socket.on('petit_nouveau', function (pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function (pseudo) {
+        pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
     });
 
-    // Dès qu'on reçoit un "message" (clic sur le bouton), on le note dans la console
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
-        // On récupère le pseudo de celui qui a cliqué dans les variables de session
-        console.log(socket.pseudo + ' me parle ! Il me dit : ' + message);
+        message = ent.encode(message);
+        socket.broadcast.emit('message', { pseudo: socket.pseudo, message: message });
     });
 });
 
+serveur.listen(8080);
 
-server.listen(8080);
